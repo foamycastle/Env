917225adbd83f15567ad506b5465ea3a5d5cc336
Aaron Sollman <unclepong@gmail.com>
2025-10-26 12:20:01 -0500

    Env::Set function
    further development of the ::Set function. The function can now have arrays passed to it.

--- src/Env.php
+++ src/Env.php
@@ -39,8 +39,32 @@ class Env
         }
     }
 
-    public static function Set(string $key, $value):void
+    /**
+     * Set an environment variable. Many variables may set from an array. If both arrays contain an equal number of elements,
+     *  each key will match with each value in the same position in each array. If the arrays contain an unequal number of elements,
+     *  only the elements with corresponding value in each array will be added and other values will be ignored.
+     * @param string|array<int,string> $key
+     * @param string|array<int,string> $value
+     * @return void
+     */
+    public static function Set(string|array $key, array|string|null $value=null):void
     {
-        $_ENV[$key] = $value;
+        if(is_string($key)) {
+            if(is_array($value) && !empty($value)){
+                $_ENV[$key] = join("; ", array_values($value)[0]);
+            }elseif(is_string($value)){
+                $_ENV[$key] = $value;
+            }
+            return;
+        }
+        if (is_array($key)) {
+            if (is_array($value) && (array_is_list($key) && array_is_list($value))) {
+                if (reset($key) !== false && reset($value) !== false) {
+                    do {
+                        $_ENV[current($key)] = current($value) ?? '';
+                    } while (next($key) !== false);
+                }
+            }
+        }
     }
 }
